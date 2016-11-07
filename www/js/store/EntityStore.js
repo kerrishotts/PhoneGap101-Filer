df6f98966660066e7c31ee9f907e6f0f8814a118
kerrishotts@gmail.com <kerrishotts@Kerris-Macbook-Pro.local>
2016-11-06 23:43:48 -0600

    More descriptive event names

--- www/js/store/EntityStore.js
+++ www/js/store/EntityStore.js
@@ -9,18 +9,18 @@ class EntityStore extends Emitter {
     }
     
     save(uuid, data) {
-        return this[_adapter].save(uuid, data).then(() => this.emit("saved", uuid));
+        return this[_adapter].save(uuid, data).then(() => this.emit("savedEntity", uuid));
     }
     
     load(uuid) {
         return this[_adapter].load(uuid).then((data) => {
-            this.emit("loaded", uuid);
+            this.emit("loadedEntity", uuid);
             return data;
         });
     }
     
     remove(uuid) {
-        return this[_adapter].remove(uuid).then(() => this.emit("removed", uuid));
+        return this[_adapter].remove(uuid).then(() => this.emit("removedEntity", uuid));
     }
     
     static make(adapter) {
